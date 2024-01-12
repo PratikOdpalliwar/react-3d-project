cf1ba99f451dd32a8e0489e9f40870465f0bc395
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-01-12 18:39:17 +0530

    SoundSection component is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Jumbotron from './components/Jumbotron'
+import SoundSection from './components/SoundSection'
 
 const App = () => {
   return (
    <div className="App">
     <Navbar/>
     <Jumbotron/>
-    
+    <SoundSection/>
    </div>
   )
 }
